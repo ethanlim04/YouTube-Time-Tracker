19ac7b1f9f03f901ffc14165275e2c310612a7cd
Ethan <hyeonseong04.lim@gmail.com>
2023-02-02 03:50:27 -0500

    Improved features (auto-save when watching another video or closing the tab)

--- public/background.ts
+++ public/background.ts
@@ -55,6 +55,25 @@
 // getVideoTitle(tabId).then((title) => {
 //   updatePlaying(tab, title, changeInfo.audible, currentTime)
 // })
+
+// const updateTabs = (): {[key: number]: {tab: chrome.tabs.Tab, title: string}} | any => {  
+//   chrome.storage.session.get("CurrentTabs").then((tabs) => {
+//     getTabs().then((res) => {
+//       let result = toYtTabs(res)
+      
+//       console.log(checkTabs(tabs["CurrentTabs"], result))
+//       if(!checkTabs(tabs["CurrentTabs"], result)) {
+//         chrome.storage.session.set({"CurrentTabs": result}).then(() => {
+//           return result
+//         })
+//       }
+//       else {
+//         return result
+//       }
+//     })
+//   })
+// }
+// {[key: number]: {tab: chrome.tabs.Tab, title: string}}
 const getTabs = async (): Promise<chrome.tabs.Tab[]> => {
   const tabs = await chrome.tabs.query({
     url: ["https://www.youtube.com/watch*"]
@@ -65,7 +84,7 @@ const getTabs = async (): Promise<chrome.tabs.Tab[]> => {
 const checkTabs = (arr1: {[key: number]: {tab: chrome.tabs.Tab, title: string}} | undefined, arr2: {[key: number]: {tab: chrome.tabs.Tab, title: string}}) => {
   // console.log("LENGTHS:", arr1?.length, arr2.length)
   if(!arr1 || Object.keys(arr2).length === 0) {
-    console.log("ARR1 NULL")
+    console.log("Storage is Null")
     return false
   }
   if(Object.keys(arr1).length != Object.keys(arr2).length) {
@@ -94,9 +113,9 @@ const getVideoTitle = (tab: chrome.tabs.Tab): string => {
   let tempTitle = String(tab.title).trim()
   if(tempTitle.length > 10) {
     if(tempTitle[0] === "(")
-      return tempTitle.substring(3, tempTitle.length - 10)
+      return tempTitle.substring(3, tempTitle.length - 10).trim()
   }
-  return tempTitle.substring(0, tempTitle.length - 10)
+  return tempTitle.substring(0, tempTitle.length - 10).trim()
 }
 
 const toYtTabs = ((arr: chrome.tabs.Tab[]):  {[key: number]: {tab: chrome.tabs.Tab, title: string}} => {
@@ -109,26 +128,6 @@ const toYtTabs = ((arr: chrome.tabs.Tab[]):  {[key: number]: {tab: chrome.tabs.T
   return res
 })
 
-
-
-// const updateTabs = (): {[key: number]: {tab: chrome.tabs.Tab, title: string}} | any => {  
-//   chrome.storage.session.get("CurrentTabs").then((tabs) => {
-//     getTabs().then((res) => {
-//       let result = toYtTabs(res)
-      
-//       console.log(checkTabs(tabs["CurrentTabs"], result))
-//       if(!checkTabs(tabs["CurrentTabs"], result)) {
-//         chrome.storage.session.set({"CurrentTabs": result}).then(() => {
-//           return result
-//         })
-//       }
-//       else {
-//         return result
-//       }
-//     })
-//   })
-// }
-// {[key: number]: {tab: chrome.tabs.Tab, title: string}}
 const updateTabs = async (): Promise<any> => {  
   let tabs = await chrome.storage.session.get("CurrentTabs")
   let result = toYtTabs(await getTabs())
@@ -148,7 +147,7 @@ chrome.tabs.onActivated.addListener((currentTab) => {
   chrome.tabs.get(currentTab.tabId).then((tab) => {
     if(tab.url?.includes("youtube.com/watch?v=")) {
       updateTabs().then((tabs) => {
-        console.log(tabs)
+        console.log("ACTIVATED TABS", tabs)
         if(!tabs[tab.id!]) tabs[tab.id!] = tab
         chrome.storage.session.set({"CurrentTabs": tabs})
       })
@@ -172,7 +171,7 @@ const updatePlaying = (tab: chrome.tabs.Tab, title: string, playingStatus: boole
         playing[title].lastPlaying = playingStatus
       }
       else if(!playingStatus && playing[title].lastPlaying) { //Playing then paused
-        playing[title].lastEnd = currentTime //- 3500
+        playing[title].lastEnd = currentTime - 3000//- 3500
         if(playing[title].lastEnd - playing[title].lastStart > 0) playing[title].totalPlayTime += playing[title].lastEnd - playing[title].lastStart
         playing[title].lastPlaying = playingStatus
       }
@@ -187,36 +186,37 @@ const updatePlaying = (tab: chrome.tabs.Tab, title: string, playingStatus: boole
 
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Change status playing??
-    let currentTime = Date.now()
-    if(changeInfo.url) {
-      // console.log("YES")
-      chrome.storage.session.get("CurrentTabs").then((tabs) => {
-        if(tabs["CurrentTabs"]) {
-          if(Object.keys(tabs["CurrentTabs"]).indexOf(String(tabId)) != -1) {
-            console.log(tabs["CurrentTabs"][tabId].title)
-            updatePlaying(tab, tabs["CurrentTabs"][tabId].title, false, currentTime)
-            updateTabs().then((currentTabs) => {
-              chrome.storage.session.set({"CurrentTabs": currentTabs})
-            })
-          }
+  // Change status playing??
+  console.log("CHANGE INFO:", changeInfo)
+  let currentTime = Date.now()
+  if(changeInfo.url) {
+    // console.log("YES")
+    chrome.storage.session.get("CurrentTabs").then((tabs) => {
+      if(tabs["CurrentTabs"]) {
+        if(Object.keys(tabs["CurrentTabs"]).indexOf(String(tabId)) != -1) {
+          console.log(tabs["CurrentTabs"][tabId].title)
+          updatePlaying(tab, tabs["CurrentTabs"][tabId].title, false, currentTime)
+          //Wait for page to load
+          //Check when tab title updates (url changes => page loads => title changes)
         }
-      })
+      }
+    })
+  }
 
-    }
-    
-    
+  if(changeInfo.title) {
     if(tab.url?.includes("youtube.com/watch?v=")) {
-    // console.log(Object.keys(changeInfo))
-    if(Object.keys(changeInfo).indexOf("audible") != -1) {
-      console.log("Audible", changeInfo.audible)
-      updatePlaying(tab, getVideoTitle(tab), changeInfo.audible, currentTime)
+      console.log("TITLE CHANGED :D")
+      updatePlaying(tab, getVideoTitle(tab), tab.audible, currentTime)
+      updateTabs()
+    }
+  }
 
-      // chrome.scripting.executeScript({target: {tabId: tabId}, files: ['getTitle.js']}).then((title) => {
-      //   console.log(title)
-      // })
-      // let title = "【誕生日に】Ubiquitous dB／湊あくあ【歌ってみた】"
-      // updatePlaying(tab, title, changeInfo.audible)
+  
+  
+  if(tab.url?.includes("youtube.com/watch?v=")) {
+    if(Object.keys(changeInfo).indexOf("audible") != -1) {
+      console.log("Audible", getVideoTitle(tab), changeInfo.audible)
+      updatePlaying(tab, getVideoTitle(tab), changeInfo.audible, currentTime)
     }
   }
 })
