1c3795ae6b507f1a56a9556e2edae9749ed8e8f8
Ethan <hyeonseong04.lim@gmail.com>
2023-02-05 02:59:35 -0500

    Improved front end UI, fixed some bugs, added option to display only currently playing videos

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,15 @@ import {
   TableBody,
   TableCell,
   TableContainer,
-  DataTableRow
+  DataTableRow,
+  TableToolbar,
+  TableToolbarContent,
+  TableToolbarMenu,
+  TableToolbarAction,
+  TableToolbarSearch,
+  Button,
+  Checkbox,
+  Toggle
 } from 'carbon-components-react'
 
 function App() {
@@ -22,6 +30,7 @@ function App() {
   const [songs, setSongs] = useState<{[title: string]: {lastEnd: number, lastPlaying: boolean, lastStart: number, totalPlayTime: number}}>()
   const [data, setData] = useState<DataTableRow<string>[]>([])
   const [totalTime, setTotalTime] = useState<number>()
+  const [hidePaused, setCheckbox] = useState<boolean>(false)
 
   useEffect(() => {
     chrome.storage.local.get("songs").then((res) => {
@@ -40,16 +49,21 @@ function App() {
           let totalPlayTime = ""
           if(res["songs"][song].lastPlaying && Date.now() > res["songs"][song].lastStart) totalPlayTime = String(Math.floor(((res["songs"][song].totalPlayTime + (Date.now() - res["songs"][song].lastStart)) / 1000) / 60)) + "M : " + String(Math.floor((res["songs"][song].totalPlayTime + (Date.now() - res["songs"][song].lastStart)) / 1000) % 60) + "S"
           else  totalPlayTime = String(Math.floor((res["songs"][song].totalPlayTime / 1000) / 60)) + "M : " + String(Math.floor(res["songs"][song].totalPlayTime / 1000) % 60) + "S"
-          // if(res["song"][song].lastPlaying) totalPlayTime = String(((res["songs"][song].totalPlayTime + (Date.now() - res["songs"][song].lastStart)) / 1000) / 60) + "M" + String(((res["songs"][song].totalPlayTime + (Date.now() - res["songs"][song].lastStart)) / 1000) % 60) + "S"
-          // else totalPlayTime = String((res["songs"][song].totalPlayTime / 1000) / 60) + "M" + String((res["songs"][song].totalPlayTime / 1000) % 60) + "S"
-          // String(Math.floor((res["songs"][song].totalPlayTime / 1000) / 60)) + "M : " + String(Math.floor(res["songs"][song].totalPlayTime / 1000) % 60) + "S"
+
           const toPush = {
             id: String(count),
             title: song,
             totalPlayTime: totalPlayTime,
             playingStatus: status
           }
-          dataArr.push(toPush)
+
+          //Only display playing when user toggles option
+          if(!hidePaused) {
+            dataArr.push(toPush)
+          }
+          else {
+            if(toPush.playingStatus === "Playing") dataArr.push(toPush)
+          }
           count += 1
         }
         setData(dataArr)
@@ -60,11 +74,12 @@ function App() {
     chrome.storage.local.get("totalTime").then((time) => {
       setTotalTime(time["totalTime"])
     })
-    
-
     return;
-  }, [])
+  // }, [])
+  }, [hidePaused])
+
   console.log("Data", data)
+  console.log("Show Playing only", hidePaused)
 
   const headerData = [
     {
@@ -87,37 +102,92 @@ function App() {
         <div className="container">
           <Tile className="content-container">
             <p>TOTAL WATCH TIME: {String(Math.floor(totalTime!/1000/60)) + "M : " + String(Math.floor(totalTime!/1000)%60) + "S"}</p>
+            {/* <Toggle
+            aria-label="toggle button"
+            defaultToggled
+            id="hide-paused"
+            labelText="Show Playing Only"
+            /> */}
+            <Checkbox labelText="Hide Paused" id="hide-paused" checked={hidePaused} onChange={() => {
+              setCheckbox(!hidePaused)
+              console.log("Status changed")
+            }}/>
             <DataTable
               rows={data}
               headers={headerData}
+              isSortable
               render={({rows, headers, getHeaderProps}) => (
                 <TableContainer title="Watched Videos">
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                {headers.map((header) => (
-                                    // eslint-disable-next-line react/jsx-key
-                                    <TableHeader {...getHeaderProps({header})}>
-                                        {header.header}
-                                    </TableHeader>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow key={row.id}>
-                                    {row.cells.map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {cell.value}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
+
+                 {/* <TableToolbar>
+                 <TableToolbarContent>
+                  <TableToolbarMenu>
+                  <TableToolbarAction>
+                  Action 1
+                  </TableToolbarAction>
+                  <TableToolbarAction>
+                  Action 2
+                  </TableToolbarAction>
+                  <TableToolbarAction>
+                  Action 3
+                  </TableToolbarAction>
+                  </TableToolbarMenu>
+                  <Button>Primary Button</Button>
+                  </TableToolbarContent>
+                  </TableToolbar> */}
+
+                  <Table>
+                      <TableHead>
+                          <TableRow>
+                              {headers.map((header) => (
+                                  // eslint-disable-next-line react/jsx-key
+                                  <TableHeader {...getHeaderProps({header})}>
+                                      {header.header}
+                                  </TableHeader>
+                              ))}
+                          </TableRow>
+                      </TableHead>
+                      <TableBody>
+                          {rows.map((row) => (
+                              <TableRow key={row.id}>
+                                  {row.cells.map((cell) => (
+                                      <TableCell key={cell.id}>
+                                          {cell.value}
+                                      </TableCell>
+                                  ))}
+                              </TableRow>
+                          ))}
+                      </TableBody>
+                  </Table>
                 </TableContainer>
               )}
             />
+              {/* <DataTable rows={data} headers={headerData} isSortable>
+                {({ rows, headers, getHeaderProps, getRowProps, getTableProps }) => (
+                  <TableContainer title="DataTable" description="With sorting">
+                    <Table {...getTableProps()}>
+                      <TableHead>
+                        <TableRow>
+                          {headers.map((header) => (
+                            <TableHeader key={header.key} {...getHeaderProps({ header })}>
+                                {header.header}
+                              </TableHeader>
+                            ))}
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {rows.map((row) => (
+                            <TableRow key={row.id} {...getRowProps({ row })}>
+                              {row.cells.map((cell) => (
+                                <TableCell key={cell.id}>{cell.value}</TableCell>
+                              ))}
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                )}
+              </DataTable> */}
           </Tile>
         </div>
       </div>
